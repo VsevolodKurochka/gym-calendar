2c0b3f08e8086d5dbd6f71d0545b1865f842b149
Vsevolod Kurochka <seva.kurochka@gmail.com>
2021-09-22 01:28:22 +0300

    fix env

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'antd/dist/antd.css';
+import 'antd-mobile/lib/calendar/style/css';
 import './scss/index.scss';
 
 ReactDOM.render(
--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,6 +1,7 @@
+import {Calendar as MobileCalendar} from 'antd-mobile';
 import {Spin, Typography, Row, Col, Calendar} from 'antd';
 const { Title } = Typography;
-
+import enUS from 'antd-mobile/lib/calendar/locale/en_US';
 
 const Home = ({loading}) => {
 
@@ -19,6 +20,11 @@ const Home = ({loading}) => {
     return (
         <>
             <Title>CALENDAR</Title>
+            <MobileCalendar
+                visible={true}
+            />
+            <br />
+            <h1>Desktop:</h1>
             <div className="home-calendar">
                 <Calendar fullscreen={false} onPanelChange={onPanelChange} onSelect={onSelect} />
             </div>
